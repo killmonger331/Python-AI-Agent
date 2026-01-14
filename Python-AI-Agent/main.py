257ed6d0aac9f960ed7cd16507938ce9787c7d80
killmonger331 <richardmorales_24@hotmail.com>
2026-01-13 19:54:56 -0800

    imported genai library, implemented use of API key

--- Python-AI-Agent/main.py
+++ Python-AI-Agent/main.py
@@ -1,13 +1,18 @@
 import os
 from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
+from google import genai
 
 def main():
-    print("Hello from python-ai-agent!")
-
-
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("GEMINI_API_KEY not found in environment variables.")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(response.text)
 if __name__ == "__main__":
     main()
+
